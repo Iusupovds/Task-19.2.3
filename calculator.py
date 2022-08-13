bb87c34050ca09e14a49f3c9f7f34d5948fec5ec
Iusupovds <105906850+Iusupovds@users.noreply.github.com>
2022-08-13 15:00:54 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,19 +1,12 @@
-import pytest
-from app.calculator import Calculator
+class Calculator:
+    def addition(self, x, y):
+        return x + y
 
+    def subtraction(self, x, y):
+        return x - y
 
-class TestCalc:
-    def setup(self):
-        self.calc = Calculator
+    def multiply(self, x, y):
+        return x * y
 
-    def test_addition_calculate_correctly(self):
-        assert self.calc.addition(self, 1, 2) == 3
-
-    def test_subtraction_calculate_correctly(self):
-        assert self.calc.subtraction(self, 3, 1) == 2
-
-    def test_multiply_calculate_correctly(self):
-        assert self.calc.multiply(self, 2, 5) == 10
-
-    def test_division_calculate_correctly(self):
-        assert self.calc.division(self, 6, 3) == 2
+    def division(self, x, y):
+        return x / y
